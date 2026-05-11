9c4d3cb4200e42c285f87254ac588a2d56786188
Atilolaoluwa Folarin <opafvn@hsd.ca>
2026-05-11 13:32:48 -0500

    Chicken

--- FadePlanet/Program.cs
+++ FadePlanet/Program.cs
@@ -17,6 +17,9 @@ namespace FadePlanet
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //Chicken
+
         }
     }
 }
