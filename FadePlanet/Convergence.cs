3dcb9e3e97942e4aa1232b3d7b15d382a9a7b51b
Atilolaoluwa Folarin <opafvn@hsd.ca>
2026-05-12 07:12:32 -0500

    Changed the Main form Name to Convergence

--- FadePlanet/Convergence.cs
+++ FadePlanet/Convergence.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FadePlanet
 {
-    public partial class Form1 : Form
+    public partial class Convergence : Form
     {
-        public Form1()
+        public Convergence()
         {
             InitializeComponent();
         }
